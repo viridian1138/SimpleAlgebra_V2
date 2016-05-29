7737c8d586f0d4901f8d54a43de1c99f3042284b
viridian1138 <viridian1138@earthlink.net>
2016-05-29 00:15:04 -0600

    Corrected Constants.

--- src/simplealgebra/constants/StandardConstants_SI_Units.java
+++ src/simplealgebra/constants/StandardConstants_SI_Units.java
@@ -1,31 +1,22 @@
 
-
-
-
 //$$strtCprt
 /**
-* Simple Algebra 
-* 
-* Copyright (C) 2014 Thornton Green
-* 
-* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
-* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License along with this program; if not, 
-* see <http://www.gnu.org/licenses>.
-* Additional permission under GNU GPL version 3 section 7
-*
-*/
+ * Simple Algebra 
+ * 
+ * Copyright (C) 2014 Thornton Green
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, 
+ * see <http://www.gnu.org/licenses>.
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ */
 //$$endCprt
 
-
-
-
-
-
 package simplealgebra.constants;
-
 
 import simplealgebra.*;
 import simplealgebra.meas.*;
@@ -45,236 +36,169 @@ import simplealgebra.meas.*;
  *
  */
 public class StandardConstants_SI_Units {
-	
-	
+
 	/**
 	 * Planck's Constant.
 	 */
-	public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> H =
-			new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( 6.62606957E-34 ) , new DoubleElem( 0.00000029E-34 ) );
-	
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> H = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(6.62606957E-34), new DoubleElem(0.00000029E-34));
+
 	/**
 	 * Planck's Constant over 2 pi.
 	 */
-	public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> HBAR =
-			new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( 1.054571726E-34 ) , new DoubleElem( 0.000000047E-34 ) );
-	
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> HBAR = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(1.054571726E-34), new DoubleElem(0.000000047E-34));
+
 	/**
 	 * Speed of light in vacuum.
 	 */
-	public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> C =
-			new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( 299792458.0 ) , new DoubleElem( 0.0 ) );
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> C = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(299792458.0), new DoubleElem(0.0));
 
 	/**
 	 * Newtonian Constant of Gravitation
 	 */
-	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> G =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					 new DoubleElem( 6.67408E-11 ) , new DoubleElem( 0.00031E-11 ) );
-
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> G = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(6.67408E-11), new DoubleElem(0.00031E-11));
 
 	/**
 	 * Boltzmann Constant
 	 */
-	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> k =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					 new DoubleElem( 1.38064852E-23 ) , new DoubleElem( 0.00000079E-23 ) );
-
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> k = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(1.38064852E-23), new DoubleElem(0.00000079E-23));
 
 	/**
 	 * Elementary Charge
 	 */
-	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> e =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					 new DoubleElem( 1.6021766208E-19 ) , new DoubleElem( 0.0000000098E-19  ) );
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> e = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(1.6021766208E-19), new DoubleElem(0.0000000098E-19));
 
 	/**
 	 * Electron Mass
 	 */
-	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> me =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					 new DoubleElem( 9.10938356E-31 ) , new DoubleElem( 0.00000011E-31  ) );
-
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> me = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(9.10938356E-31), new DoubleElem(0.00000011E-31));
 
 	/**
 	 * Proton Mass
 	 */
-	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> mp =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					 new DoubleElem( 1.672621898E-27 ) , new DoubleElem( 0.000000021E-27 ) );
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> mp = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(1.672621898E-27), new DoubleElem(0.000000021E-27));
 
+	// Standard numeric constants. Uncertainty represents the accuracy of the
+	// JVM-defined constants.
 
-	
-	// Standard numeric constants.  Uncertainty represents the accuracy of the JVM-defined constants.
-	
-	
 	/**
 	 * Pi.
 	 */
-	public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> PI =
-			new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( Math.PI ) , new DoubleElem( 1E-15 ) );
-	
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> PI = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(Math.PI), new DoubleElem(1E-15));
+
 	/**
 	 * E.
 	 */
-	public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> E =
-			new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( Math.E ) , new DoubleElem( 1E-15 ) );
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> E = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(Math.E), new DoubleElem(1E-15));
 
 	/**
 	 * The value of the number four, used to calculate other constants.
 	 */
-	private static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> FOUR =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( 4.0 ) , new DoubleElem( 0.0 ) );
-	
+	private static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> FOUR = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(4.0), new DoubleElem(0.0));
+
 	/**
 	 * The value of the number 1E-7, used to calculate other constants.
 	 */
-	private static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> EM7 =
-			 new ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory>(
-					new DoubleElem( 1E-7 ) , new DoubleElem( 0.0 ) );
-
+	private static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> EM7 = new ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory>(
+			new DoubleElem(1E-7), new DoubleElem(0.0));
 
 	// Constants that require the use of other constants such as PI.
 
-
 	/**
-	 * Calculates the Coulomb Constant.
-	 * @return The Coulomb Constant.
+	 * Calculates the Gravitational Permitivity.
+	 * 
+	 * @return The Gravitational Permitivity.
 	 */
-	private static ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> calcCoul()
-	{
-		try
-		{
-			return( ( PI.mult( FOUR ).mult( epsilon_0 ) ).invertLeft() );
-		}
-		catch( NotInvertibleException ex )
-		{
-			throw( new RuntimeException( "Failed" ) );
+	private static ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> calcEpsilon_g() {
+		try {
+			return ((PI.mult(FOUR).mult(G)).invertLeft().negate());
+		} catch (NotInvertibleException ex) {
+			throw (new RuntimeException("Failed"));
 		}
 	}
 
+	/**
+	 * Gravitational Permitivity of Free Space.
+	 */
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> epsilon_g = calcEpsilon_g();
 
-     	/**
-         * Coulomb Constant
-         */
-         public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> Coul =
-                         calcCoul();
+	/**
+	 * Calculates the Magnetic Permeability.
+	 * 
+	 * @return The Magnetic Permeability.
+	 */
+	private static ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> calcMu_0() {
+		return (EM7.mult(FOUR).mult(PI));
+	}
 
-         
-         
-         /**
-     	 * Calculates the Gravitational Permitivity.
-     	 * @return The Gravitational Permitivity.
-     	 */
-     	private static ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> calcEpsilon_g()
-     	{
-     		try
-     		{
-     			return( ( PI.mult( FOUR ).mult( G ) ).invertLeft().negate() );
-     		}
-     		catch( NotInvertibleException ex )
-     		{
-     			throw( new RuntimeException( "Failed" ) );
-     		}
-     	}
-     	
-     	
-     	
-     	/**
-         * Gravitational Permitivity of Free Space.
-         */
-         public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> epsilon_g =
-                         calcEpsilon_g();
-         
-         
-         
-         
-         /**
-      	 * Calculates the Magnetic Permeability.
-      	 * @return The Magnetic Permeability.
-      	 */
-      	private static ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> calcMu_0()
-      	{
-      		try
-      		{
-      			return( ( EM7.mult( FOUR ).mult( PI ) ).invertLeft().negate() );
-      		}
-      		catch( NotInvertibleException ex )
-      		{
-      			throw( new RuntimeException( "Failed" ) );
-      		}
-      	}
-      	
-      	
-      	
-      	 /**
-          * Magnetic Permeability of Free Space.
-          */
-          public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> mu_0 =
-                          calcMu_0();
-          
-          
-          
-          
-            /**
-        	 * Calculates the Gravitomagnetic Permeability (presumed).
-        	 * @return The Gravitomagnetic Permeability (presumed).
-        	 */
-        	private static ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> calcMu_g()
-        	{
-        		try
-        		{
-        			return( ( FOUR.mult( PI ).mult( G ) ).mult( ( C.mult( C ) ).invertLeft() ).negate() );
-        		}
-        		catch( NotInvertibleException ex )
-        		{
-        			throw( new RuntimeException( "Failed" ) );
-        		}
-        	}
-        	
-        	
-        	
-        	
-        	/**
-             * Gravitomagnetic Permeability of Free Space (presumed).
-             */
-             public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> mu_g =
-                             calcMu_g();
-             
-             
-             
-             /**
-         	 * Calculates the Electrical Permitivity.
-         	 * @return The Electrical Permitivity.
-         	 */
-         	private static ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> calcEpsilon_0()
-         	{
-         		try
-         		{
-         			return( ( C.mult( C ).mult( mu_0 ) ).invertLeft() );
-         		}
-         		catch( NotInvertibleException ex )
-         		{
-         			throw( new RuntimeException( "Failed" ) );
-         		}
-         	}
-             
-             
-         	
-             /**
-         	 * Electrical Permitivity of Free Space.
-         	 */
-         	 public static final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> epsilon_0 =
-         			 calcEpsilon_0();
+	/**
+	 * Magnetic Permeability of Free Space.
+	 */
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> mu_0 = calcMu_0();
 
+	/**
+	 * Calculates the Gravitomagnetic Permeability (presumed).
+	 * 
+	 * @return The Gravitomagnetic Permeability (presumed).
+	 */
+	private static ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> calcMu_g() {
+		try {
+			return ((FOUR.mult(PI).mult(G)).mult((C.mult(C)).invertLeft())
+					.negate());
+		} catch (NotInvertibleException ex) {
+			throw (new RuntimeException("Failed"));
+		}
+	}
 
+	/**
+	 * Gravitomagnetic Permeability of Free Space (presumed).
+	 */
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> mu_g = calcMu_g();
+
+	/**
+	 * Calculates the Electrical Permitivity.
+	 * 
+	 * @return The Electrical Permitivity.
+	 */
+	private static ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> calcEpsilon_0() {
+		try {
+			return ((C.mult(C).mult(mu_0)).invertLeft());
+		} catch (NotInvertibleException ex) {
+			throw (new RuntimeException("Failed"));
+		}
+	}
+
+	/**
+	 * Electrical Permitivity of Free Space.
+	 */
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> epsilon_0 = calcEpsilon_0();
+
+	/**
+	 * Calculates the Coulomb Constant.
+	 * 
+	 * @return The Coulomb Constant.
+	 */
+	private static ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> calcCoul() {
+		try {
+			return ((PI.mult(FOUR).mult(epsilon_0)).invertLeft());
+		} catch (NotInvertibleException ex) {
+			throw (new RuntimeException("Failed"));
+		}
+	}
+
+	/**
+	 * Coulomb Constant
+	 */
+	public static final ValueWithUncertaintyElem<DoubleElem, DoubleElemFactory> Coul = calcCoul();
 
 }
-
-
---
+++ src/test_simplealgebra/TestStandardConstantsSI.java
@@ -0,0 +1,117 @@
+
+
+
+
+
+
+
+//$$strtCprt
+/**
+* Simple Algebra 
+* 
+* Copyright (C) 2014 Thornton Green
+* 
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program; if not, 
+* see <http://www.gnu.org/licenses>.
+* Additional permission under GNU GPL version 3 section 7
+*
+*/
+//$$endCprt
+
+
+
+
+
+
+
+package test_simplealgebra;
+
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import simplealgebra.DoubleElem;
+import simplealgebra.DoubleElemFactory;
+import simplealgebra.constants.StandardConstants_SI_Units;
+import simplealgebra.meas.ValueWithUncertaintyElem;
+
+
+
+/**
+ * Verifies the set of SI constants.
+ * 
+ * @author thorngreen
+ *
+ */
+public class TestStandardConstantsSI extends TestCase 
+{
+
+	
+	
+	/**
+	 * Runs the test.
+	 */
+	public void testStandardConstantsSI()
+	{
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hh = StandardConstants_SI_Units.H;
+		
+		Assert.assertEquals( hh.getValue().getVal() , 6.62E-34 , 0.01E-34 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hhbar = StandardConstants_SI_Units.HBAR;
+		
+		Assert.assertEquals( hhbar.getValue().getVal() , 1.05E-34 , 0.01E-34 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hc = StandardConstants_SI_Units.C;
+		
+		Assert.assertEquals( hc.getValue().getVal() , 3.0E+8 , 0.1E+8 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hg = StandardConstants_SI_Units.G;
+		
+		Assert.assertEquals( hg.getValue().getVal() , 6.67E-11 , 0.01E-11 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hk = StandardConstants_SI_Units.k;
+		
+		Assert.assertEquals( hk.getValue().getVal() , 1.38E-23 , 0.01E-23 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> he = StandardConstants_SI_Units.e;
+		
+		Assert.assertEquals( he.getValue().getVal() , 1.60E-19 , 0.01E-19 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hme = StandardConstants_SI_Units.me;
+		
+		Assert.assertEquals( hme.getValue().getVal() , 9.1E-31 , 0.1E-31 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hmp = StandardConstants_SI_Units.mp;
+		
+		Assert.assertEquals( hmp.getValue().getVal() , 1.67E-27 , 0.01E-27 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hPI = StandardConstants_SI_Units.PI;
+		
+		Assert.assertEquals( hPI.getValue().getVal() , 3.14 , 0.01 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hE = StandardConstants_SI_Units.E;
+		
+		Assert.assertEquals( hE.getValue().getVal() , 2.7 , 0.1 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hmu_0 = StandardConstants_SI_Units.mu_0;
+		
+		Assert.assertEquals( hmu_0.getValue().getVal() , 12.566E-7 , 0.001E-7 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hep_0 = StandardConstants_SI_Units.epsilon_0;
+		
+		Assert.assertEquals( hep_0.getValue().getVal() , 8.85E-12 , 0.01E-12 );
+		
+		final ValueWithUncertaintyElem<DoubleElem,DoubleElemFactory> hcoul = StandardConstants_SI_Units.Coul;
+		
+		Assert.assertEquals( hcoul.getValue().getVal() , 8.99E+9 , 0.01E+9 );
+		
+		
+	}
+	
+	
+}
+
+
