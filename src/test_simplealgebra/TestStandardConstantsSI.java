5ce64db79f955bae6e568fa1971bbe4db2d2a4ff
viridian1138 <viridian1138@earthlink.net>
2016-06-03 22:47:39 -0600

    Refined Test.

--- src/test_simplealgebra/TestStandardConstantsSI.java
+++ src/test_simplealgebra/TestStandardConstantsSI.java
@@ -43,8 +43,6 @@ import simplealgebra.bigfixedpoint.Precision;
 import simplealgebra.constants.StandardConstants_SI_Units;
 import simplealgebra.constants.StandardConstants_SI_Units_BigFixed;
 import simplealgebra.meas.ValueWithUncertaintyElem;
-import simplealgebra.prec.DefaultPrecedenceComparator;
-import test_simplealgebra.TestMandelbrotSet.LrgPrecision;
 
 
 
@@ -110,7 +108,7 @@ public class TestStandardConstantsSI extends TestCase
      * <mrow>
      *   <msup>
      *           <mn>10</mn>
-     *         <mn>801</mn>
+     *         <mn>8001</mn>
      *   </msup>
      * </mrow>
      * </math>.
@@ -124,7 +122,7 @@ public class TestStandardConstantsSI extends TestCase
      * </mrow>
      * </math>.
 	 */
-	static final BigInteger baseVal = calcVal( 800 );
+	static final BigInteger baseVal = calcVal( 8000 );
 	
 	
 	/**
@@ -132,7 +130,7 @@ public class TestStandardConstantsSI extends TestCase
      * <mrow>
      *   <msup>
      *           <mn>10</mn>
-     *         <mn>1602</mn>
+     *         <mn>16002</mn>
      *   </msup>
      * </mrow>
      * </math>.
@@ -145,7 +143,7 @@ public class TestStandardConstantsSI extends TestCase
      * <mrow>
      *   <msup>
      *           <mn>10</mn>
-     *         <mn>801</mn>
+     *         <mn>8001</mn>
      *   </msup>
      * </mrow>
      * </math>.
@@ -175,6 +173,38 @@ public class TestStandardConstantsSI extends TestCase
 	 */
 	static final LrgPrecision lrgPrecision = new LrgPrecision();
 	
+	
+	
+	
+	/**
+	 * Returns whether the absolute value of one parameter is less than or equal to another parameter.
+	 * 
+	 * @param a First value to be compared.
+	 * @param b Second value to be compared.
+	 * @return True iff. abs( a ) <= b.
+	 */
+	protected static boolean isAbsoluteLessThan( BigFixedPointElem<LrgPrecision> a , BigFixedPointElem<LrgPrecision> b )
+	{
+		boolean ret = ( a.getPrecVal().abs() ).compareTo( b.getPrecVal() ) <= 0;
+		return( ret );
+	}
+	
+	
+	
+	/**
+	 * Returns whether a value matches within a tolerance.
+	 * 
+	 * @param a The value to compare.
+	 * @param b The value to be tested against.
+	 * @param c The tolerance.
+	 * @return True iff. abs( a - b ) <= c
+	 */
+	protected static boolean isAbsoluteLessThan( BigFixedPointElem<LrgPrecision> a , double b , double c )
+	{
+		BigFixedPointElem<LrgPrecision> bb = new BigFixedPointElem<LrgPrecision>( b, lrgPrecision );
+		BigFixedPointElem<LrgPrecision> cc = new BigFixedPointElem<LrgPrecision>( c, lrgPrecision );
+		return( isAbsoluteLessThan( a.add( bb.negate() ) , cc ) );
+	}
 	
 	
 	
@@ -251,7 +281,55 @@ public class TestStandardConstantsSI extends TestCase
 		
 		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hh = cnst.getH();
 		
-		hh.writeMathML( new DefaultPrecedenceComparator() , System.out );
+		Assert.assertTrue( isAbsoluteLessThan( hh.getValue() , 6.62E-34 , 0.01E-34 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hhbar = cnst.getHbar();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hhbar.getValue() , 1.05E-34 , 0.01E-34 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hc = cnst.getC();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hc.getValue() , 3.0E+8 , 0.1E+8 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hg = cnst.getG();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hg.getValue() , 6.67E-11 , 0.01E-11 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hk = cnst.getK();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hk.getValue() , 1.38E-23 , 0.01E-23 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> he = cnst.getQ_E();
+		
+		Assert.assertTrue( isAbsoluteLessThan( he.getValue() , 1.60E-19 , 0.01E-19 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hme = cnst.getM_E();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hme.getValue() , 9.1E-31 , 0.1E-31 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hmp = cnst.getM_P();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hmp.getValue() , 1.67E-27 , 0.01E-27 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hPI = cnst.getPi();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hPI.getValue() , 3.14 , 0.01 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hE = cnst.getE();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hE.getValue() , 2.7 , 0.1 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hmu_0 = cnst.getMu_0();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hmu_0.getValue() , 12.566E-7 , 0.001E-7 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hep_0 = cnst.getEpsilon_0();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hep_0.getValue() , 8.85E-12 , 0.01E-12 ) );
+		
+		final ValueWithUncertaintyElem<BigFixedPointElem<LrgPrecision>,BigFixedPointElemFactory<LrgPrecision>> hcoul = cnst.getCoul();
+		
+		Assert.assertTrue( isAbsoluteLessThan( hcoul.getValue() , 8.99E+9 , 0.01E+9 ) );
 		
 		
 	}
