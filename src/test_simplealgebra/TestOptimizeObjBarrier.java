53930b2989ecdfa50e739447d4c9973b3fdb107b
Thornton Green <thornton.green@gmail.com>
2018-07-01 22:30:59 -0600

    Added Test.
    Added Test.

---
+++ src/test_simplealgebra/TestOptimizeObjBarrier.java
@@ -0,0 +1,255 @@
+
+
+
+
+
+
+
+//$$strtCprt
+/**
+* Simple Algebra 
+* 
+* Copyright (C) 2014 Thornton Green
+* 
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program; if not, 
+* see <http://www.gnu.org/licenses>.
+* Additional permission under GNU GPL version 3 section 7
+*
+*/
+//$$endCprt
+
+
+
+
+
+
+
+package test_simplealgebra;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import simplealgebra.ComplexElem;
+import simplealgebra.ComplexElemFactory;
+import simplealgebra.DoubleElem;
+import simplealgebra.DoubleElemFactory;
+import simplealgebra.Elem;
+import simplealgebra.NotInvertibleException;
+import simplealgebra.WriteElemCache;
+import simplealgebra.symbolic.MultiplicativeDistributionRequiredException;
+import simplealgebra.symbolic.SCacheKey;
+import simplealgebra.symbolic.SymbolicElem;
+import simplealgebra.symbolic.SymbolicElemFactory;
+import simplealgebra.symbolic.SymbolicReduction;
+import simplealgebra.symbolic.SymbolicZero;
+
+import java.io.*;
+
+
+
+/**
+ * Verifies the ability to optimize through the so-called "object barrier".
+ * Essentially the "object barrier" refers to the inability of an optimization
+ * process to optimize algebra operations through a container object.
+ * For instance, when a complex number class of doubles is used to contain
+ * real and imaginary doubles, there usually is no standard optimization that can
+ * remove all computations of imaginary terms in the special case where the
+ * computation is entirely over the real part.  Similar limitations exist
+ * when computing with vector classes containing doubles.
+ * 
+ * This test verifies that such an optimization can be performed when a
+ * symbolic layer is inserted between the complex number and the double.
+ * 
+ * This documentation should be viewed using Firefox version 33.1.1 or above.
+ * 
+ * @author tgreen
+ *
+ */
+public class TestOptimizeObjBarrier extends TestCase 
+{
+
+	
+	/**
+	 * Node representing the variable to be tested in the expression.
+	 * 
+	 * @author thorngreen
+	 *
+	 */
+	private static class AElem extends SymbolicElem<DoubleElem,DoubleElemFactory>
+	{
+
+		/**
+		 * Constructs the node.
+		 * 
+		 * @param _fac The factory for the enclosed type.
+		 */
+		public AElem(DoubleElemFactory _fac ) {
+			super(_fac);
+		}
+
+		@Override
+		public DoubleElem eval( HashMap<? extends Elem<?,?>,? extends Elem<?,?>> implicitSpace ) throws NotInvertibleException,
+				MultiplicativeDistributionRequiredException {
+			throw( new RuntimeException( "NotSupported" ) );
+		}
+		
+		@Override
+		public DoubleElem evalCached(
+				HashMap<? extends Elem<?, ?>, ? extends Elem<?, ?>> implicitSpace,
+				HashMap<SCacheKey<DoubleElem, DoubleElemFactory>, DoubleElem> cache)
+				throws NotInvertibleException,
+				MultiplicativeDistributionRequiredException {
+			throw( new RuntimeException( "NotSupported" ) );
+		}
+
+		@Override
+		public DoubleElem evalPartialDerivative(ArrayList<? extends Elem<?, ?>> withRespectTo , HashMap<? extends Elem<?,?>,? extends Elem<?,?>> implicitSpace)
+				throws NotInvertibleException,
+				MultiplicativeDistributionRequiredException {
+			throw( new RuntimeException( "NotSupported" ) );
+		}
+		
+		@Override
+		public DoubleElem evalPartialDerivativeCached(
+				ArrayList<? extends Elem<?, ?>> withRespectTo,
+				HashMap<? extends Elem<?, ?>, ? extends Elem<?, ?>> implicitSpace,
+				HashMap<SCacheKey<DoubleElem, DoubleElemFactory>, DoubleElem> cache)
+				throws NotInvertibleException,
+				MultiplicativeDistributionRequiredException {
+			throw( new RuntimeException( "NotSupported" ) );
+		}
+		
+		@Override
+		public boolean symbolicEquals( SymbolicElem<DoubleElem,DoubleElemFactory> b )
+		{
+			if( b instanceof AElem )
+			{
+				return( true );
+			}
+			
+			return( false );
+		}
+
+		@Override
+		public String writeDesc(
+				WriteElemCache<SymbolicElem<DoubleElem, DoubleElemFactory>, SymbolicElemFactory<DoubleElem, DoubleElemFactory>> cache,
+				PrintStream ps) {
+			String st = cache.get( this );
+			if( st == null )
+			{
+				final String sta = fac.writeDesc( (WriteElemCache<DoubleElem,DoubleElemFactory>)( cache.getInnerCache() ) , ps);
+				st = cache.getIncrementVal();
+				cache.put(this, st);
+				ps.print( AElem.class.getSimpleName() );
+				ps.print( " " );
+				ps.print( st );
+				ps.print( " = new " );
+				ps.print( AElem.class.getSimpleName() );
+				ps.print( "( " );
+				ps.print( sta );
+				ps.println( " );" );
+			}
+			return( st );
+		}
+		
+		
+	}
+	
+	
+	
+	
+	/**
+	 * A symbolic elem representing a constant value
+	 * at the base Newton-Raphson evaluation level.
+	 * 
+	 * @author thorngreen
+	 *
+	 */
+	private static class SymbolicConst extends SymbolicReduction<DoubleElem, DoubleElemFactory>
+	{
+
+		/**
+		 * Constructs the elem.
+		 * 
+		 * @param _elem The constant to be represented.
+		 * @param _fac The factory for the constant.
+		 */
+		public SymbolicConst(DoubleElem _elem, DoubleElemFactory _fac) {
+			super(_elem, _fac);
+		}
+		
+		@Override
+		public boolean symbolicEquals( SymbolicElem<DoubleElem,DoubleElemFactory> b )
+		{
+			if( b instanceof SymbolicConst )
+			{
+				return( getElem().getVal() == ( (SymbolicConst) b ).getElem().getVal() );
+			}
+			return( false );
+		}
+		
+	}
+	
+	
+	
+	
+	/**
+	 * Runs the test.
+	 * 
+	 * @throws NotInvertibleException
+	 */
+	public void testOptimizeObjBarrier() throws NotInvertibleException
+	{
+		
+		final DoubleElemFactory dl = new DoubleElemFactory();
+		
+		final SymbolicElemFactory<DoubleElem,DoubleElemFactory> se = 
+				new SymbolicElemFactory<DoubleElem,DoubleElemFactory>(dl);
+		
+		final ComplexElemFactory<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> ce =
+				new ComplexElemFactory<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>>( se );
+		
+		final SymbolicElem<DoubleElem,DoubleElemFactory>
+			a0 = new AElem( dl );
+		
+		final ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> a00
+			= new ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>>( a0 , se.zero() );
+		
+		ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> f = ce.zero();
+		
+		for( int cnt = 1 ; cnt < 11 ; cnt++ )
+		{
+			ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> f0 = a00;
+			for( int cnt1 = 0 ; cnt1 < ( cnt - 1 ) ; cnt1++ )
+			{
+				f0 = f0.mult( a00 );
+			}
+			
+			final SymbolicConst cnst = new SymbolicConst( new DoubleElem( cnt + 1 ) , dl );
+			
+			final ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> coeff
+				= new ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>>( cnst , se.zero() ); // Trailing coefficient
+			
+			f0 = f0.mult( coeff );
+			
+			f = f.add( f0 );
+		}
+		
+		final ComplexElem<SymbolicElem<DoubleElem,DoubleElemFactory>,SymbolicElemFactory<DoubleElem,DoubleElemFactory>> f1
+			= f.distributeSimplify2();
+		
+		Assert.assertTrue( f1.getIm() instanceof SymbolicZero ); // Imaginary terms eliminated because computation is entirely over real.
+		
+	}
+	
+	
+}
+
+
